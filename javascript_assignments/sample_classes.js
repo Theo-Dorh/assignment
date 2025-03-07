523e2cdcde7543b5a000c68d3accb3b6841cc0b6
Theo-Dorh <dorhtheophilus@gmail.com>
2025-03-07 19:08:43 +0000

    Edited calculator assignment and added js assignment

---
+++ javascript_assignments/sample_classes.js
@@ -0,0 +1,74 @@
+class OpenBook {
+    head;
+    tail;
+    middle;
+
+    open (head){
+        // steps
+    }
+
+    close (tail) {
+        //steps
+    }
+}
+
+class Cook {
+    gas;
+    pan;
+    bowl;
+
+    heat (pan) {
+        //steps
+    }
+
+    fry (pan) {
+        //steps
+    }
+}
+
+class DrivingManual {
+    ignition;
+    accelerator;
+    brake;
+    gear;
+
+    start (ignition) {
+        //steps
+    }
+
+    acelerate (accelerator) {
+        //steps
+    }
+
+    reverse (gear) {
+        //steps
+    }
+}
+
+class TankFiller {
+    tank;
+    tankCover;
+    tankNeck;
+
+    startFilling (tank) {
+        //steps
+    }
+
+    shutOff (tankNeck) {
+        //steps
+    }
+}
+
+class DishWasher {
+    dishes;
+    sink;
+    runningWater;
+
+    openTap (runningWater) {
+        //steps
+    }
+
+    washDishes (dishes) {
+        //steps
+    }
+}
